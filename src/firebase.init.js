aa85c0783e52c5bf9774f4ed50c222bb0654e28a
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-01-27 23:29:53 +0600

    first commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDO_MAIN,
+  apiKey: process.env.REACT_APP_REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGS_ENDERIL,
-  appId: process.env.REACT_APP_APP_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
