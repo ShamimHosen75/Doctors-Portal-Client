469a1578f5089940a27baf9e01249d6fa62275c4
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-01-29 02:45:06 +0600

    first commit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,78 @@
 import React from "react";
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
 import auth from "../../firebase.init";
 
-const Login = () => {
-   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+ const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    } = useForm();
 
-   if(user){
-    console.log(user);
-   }
+    if (user) {
+      console.log(user);
+    }
 
-  return (
-   <div className="flex h-screen justify-center items-center">
-     <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="text-center text-2xl font-bold">Login</h2>
+    const onSubmit = data => {
+      console.log(data);
+    };
+ 
+ return (
+  <div className="flex h-screen justify-center items-center">
+   <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card-body">
+     <h2 className="text-center text-2xl font-bold">Login</h2>
+     <form onSubmit={handleSubmit(onSubmit)}>
 
-          <div className="divider">OR</div>
-          <button 
-            onClick={() => signInWithGoogle()}
-            className="btn btn-outline"
-          >Continue with Google</button>
-        </div>
-     </div>
+     <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">Email</span>
+        </label>
+        <input 
+            type="email" 
+            placeholder="Enter Your Email" 
+            className="input input-bordered w-full max-w-xs"
+            {...register("email", {
+              pattern: {
+                value: /[A-Za-z]{3}/,
+                message: 'error message'
+              }
+            })}
+            />
+        <label className="label">
+          <span className="label-text-alt">Alt label</span>
+        </label>
+    </div>
+
+
+
+
+        <input
+        
+        aria-invalid={errors.firstName ? "true" : "false"}
+        />
+        {errors.firstName?.type === "required" && (
+        <p role="alert">First name is required</p>
+        )}
+
+        <input
+        {...register("mail", { required: "Email Address is required" })}
+        aria-invalid={errors.mail ? "true" : "false"}
+        />
+        {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+
+        <input type="submit" />
+     </form>
+     <div className="divider">OR</div>
+      <button onClick={() => signInWithGoogle()} 
+        className="btn btn-outline">
+        Continue with Google
+      </button>
+    </div>
    </div>
+  </div>
  );
 };
 
